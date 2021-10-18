87e38b5ce071c071179b1af0a8af721b55080c7e
Aman7445 <ak858696@gmail.com>
2021-10-19 01:42:40 +0530

    updated jsx

--- sample.tsx
+++ sample.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export class code extends React.Component<any, any> {
   render() {
-    return <div>added jsp, code much</div>;
+    return <div>added jsx, code much</div>;
   }
 }
